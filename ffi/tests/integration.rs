4d0f2d1d9169b3e2293a5a1a206b98ecd4ff5317
Danilo Bargen <mail@dbrgn.ch>
2018-01-31 16:45:38 +0100

    Integration tests: More diagnostics

--- ffi/tests/integration.rs
+++ ffi/tests/integration.rs
@@ -2,17 +2,25 @@
 extern crate lazy_static;
 
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Command, Output};
 use std::sync::{Mutex, MutexGuard};
 
 lazy_static! {
     static ref C_TEST_MUTEX: Mutex<()> = Mutex::new(());
 }
 
+fn assert_output_success(output: Output) {
+    if !output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        println!("Stdout:\n{}\nStderr:\n{}\n", stdout, stderr);
+        panic!("Running C tests failed with non-zero return code");
+    }
+}
+
 fn build_tests() -> (MutexGuard<'static, ()>, PathBuf) {
     let guard = C_TEST_MUTEX.lock().unwrap();
 
-    let root_dir = env!("CARGO_MANIFEST_DIR");
     let out_dir = env!("OUT_DIR");
     let build_dir = Path::new(out_dir).join("build");
 
@@ -21,14 +29,14 @@ fn build_tests() -> (MutexGuard<'static, ()>, PathBuf) {
         .arg(build_dir.to_str().unwrap())
         .output()
         .expect("Could not run meson to build C tests");
-    assert!(output.status.success());
+    assert_output_success(output);
 
     println!("Running ninja...");
     let output = Command::new("ninja")
         .current_dir(&build_dir)
         .output()
         .expect("Could not run ninja to build C tests");
-    assert!(output.status.success());
+    assert_output_success(output);
 
     (guard, build_dir)
 }
